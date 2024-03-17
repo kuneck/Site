57e15c57b2131e2c9abd91ad1688abd4e4bc5518
kuneck <kuneck@gmail.com>
2024-03-17 12:07:26 +0000

    Add project files.

---
+++ Site/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+// Functions to change text color
+function newColor() {
+    document.getElementById('favorite_song').style.color = 'gold';
+}
+
+function oldColor() {
+    document.getElementById('favorite_song').style.color = 'whitesmoke';
+}
